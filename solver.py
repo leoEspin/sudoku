e518c89b18e5901043284595d2a351cd001cb235
leoEspin <35843183+leoEspin@users.noreply.github.com>
2022-06-13 08:18:12 -0400

    removed unnecessary function calls made several methods hidden

--- solver.py
+++ solver.py
@@ -9,13 +9,14 @@ class sudoku():
         self.size = N
         self.board = np.zeros((9,9), dtype=int)
         self.indices = [(i, j) for i in range(N) for j in range(N)]
+        self.cells = {index: self._get_cell(index) for index in self.indices}
         currOS = platform.system()
         if currOS == 'Windows':
             os.system('cls')
         else:
             os.system('clear')
         #delay for screen output in seconds. Can be changed with setDelay
-        self.delay = 0.1 
+        self.delay = 0.05 
         self.toString()
 
     def setDelay(self,secs):
@@ -23,11 +24,11 @@ class sudoku():
             
     def toString(self):
         '''
-        Prints the current status of the chess board on the screen
+        Prints the current status of the board on the screen
         '''
         output=[]
-        row=['*'] + ['-' for k in range(self.size)] + ['*']
-        hedge=[' '] + ['*' for k in range(self.size)] + [' ']
+        row=['|'] + ['-' for k in range(self.size)] + ['|']
+        hedge=['+'] + ['-' for k in range(self.size)] + ['+']
         output.append(hedge)
         for i in range(self.size):
             output.append(row.copy())
@@ -60,27 +61,26 @@ class sudoku():
         self.toString()
         return
 
-    def get_cell(self, index: tuple):
+    def _get_cell(self, index: tuple):
         '''coordinates of index' sqrt(N)xsqrt(N) cell (default 3x3 )'''
         cell_size = int(np.sqrt(self.size))
         return (index[0] // cell_size, index[1] // cell_size)
 
-    def get_affected_indices(self, index: tuple)-> list:
+    def _get_affected_indices(self, index: tuple)-> list:
         '''affected positions according to sudoku rules'''
-        curr_cell = self.get_cell(index)
         rows = {(index[0], j) for j in range(self.size)}
         cols = {(j, index[1]) for j in range(self.size)}
         cell = {
             (i, j)  for i in range(self.size) for j in range(self.size)\
-                if self.get_cell((i, j)) == curr_cell
+                if self.cells[(i, j)] == self.cells[index]
         }
         return list(
             rows.union(cols).union(cell)
         )
     
-    def get_occupied_vals(self, index: tuple)-> list:
+    def _get_occupied_vals(self, index: tuple)-> list:
         '''what are the used values according to sudoku rules'''
-        values = {self.board[tup] for tup in self.get_affected_indices(index)}
+        values = {self.board[tup] for tup in self._get_affected_indices(index)}
         return [x for x in values if x != 0]
 
     def get_candidates(self)-> dict:
@@ -89,7 +89,7 @@ class sudoku():
         for index in self.indices:
             if self.board[index] == 0:
                 candidates[index] = [
-                    x for x in range(1, 10) if x not in self.get_occupied_vals(index)
+                    x for x in range(1, 10) if x not in self._get_occupied_vals(index)
                 ]
         return candidates
 
@@ -105,7 +105,7 @@ class sudoku():
         '''
         candidates = deepcopy(candidates)
         self.board[index] = fill
-        indices_to_check = self.get_affected_indices(index)
+        indices_to_check = self._get_affected_indices(index)
         for tup in indices_to_check:
             if tup in candidates and fill in candidates[tup]:
                 candidates[tup].remove(fill)
@@ -121,12 +121,14 @@ class sudoku():
             [key for key in indices if key not in candidates.keys()]
         ) == 0    
 
-    def count_empties(self)-> int:
+    def _count_empties(self)-> int:
         return len([1 for x in self.indices if self.board[x] == 0])
 
-    def fill_board(self, candidates: dict)-> bool:
-        if self.count_empties() == 0:
+    def fill_board(self, candidates: dict = None)-> bool:
+        if self._count_empties() == 0:
             return True
+        if candidates == None:
+            candidates = self.get_candidates()
         if not self.is_viable(candidates):
             return False
         position, values = candidates.popitem()
@@ -164,4 +166,4 @@ if __name__ == '__main__':
     test.load_board(
         '5...8..49...5...3..673....115..........2.8..........187....415..3...2...49..5...3'
     )
-    test.fill_board(test.get_candidates())
+    test.fill_board()
